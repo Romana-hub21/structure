316d12c728d6087dec2234d5be5de26ea4c32fe1
Romana-hub21 <ummeromana21@gmail.com>
2025-05-09 12:03:35 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import StructiPro from './StructiPro';
+
+function App() {
+  return <StructiPro />;
+}
+
+export default App;
---
+++ StructiPro.js
@@ -0,0 +1,3 @@
+
+// StructiPro.js
+// This file contains the full StructiPro component (code omitted here for brevity in the output)
